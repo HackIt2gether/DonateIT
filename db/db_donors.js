d0dda270d4003f93fbad94d24828216dce057de2
emineKoc <eminekoc@gmail.com>
2016-05-15 17:50:13 -0400

    add

--- db/db_donors.js
+++ db/db_donors.js
@@ -9,8 +9,8 @@ const cn = process.env.DATABASE_URL;
 const db = pgp(cn);
 
 function addDonation(req, res, next) {
-  db.one(`INSERT INTO donors (name, email, pickup_address, category, item_description)
-  VALUES ($1, $2, $3, $4, $5)`,
+  db.ones(`INSERT INTO donors (name, email, pickup_address, category, item_description)
+  VALUES ($1, $2, $3, $4, $5) RETURNING *`,
     [req.body.name, req.body.email, req.body.pickup_address, req.body.category, req.body.item_description])
     .then(function(data) {
       console.log(data);
