7e18411af50b01954848fa25a11573b0aa429a18
LGOLIK <golikster@gmail.com>
2016-05-15 17:59:10 -0400

    more about additions

--- db/db_donors.js
+++ db/db_donors.js
@@ -10,7 +10,7 @@ const db = pgp(cn);
 
 function addDonation(req, res, next) {
   db.one(`INSERT INTO donors (name, email, pickup_address, category, item_description)
-  VALUES ($1, $2, $3, $4, $5)`,
+  VALUES ($1, $2, $3, $4, $5) RETURNING *`,
     [req.body.name, req.body.email, req.body.pickup_address, req.body.category, req.body.item_description])
     .then(function(data) {
       console.log(data);
