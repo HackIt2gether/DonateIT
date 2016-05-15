4ceb60680ef5df3901686d73f18f78cecc560065
LGOLIK <golikster@gmail.com>
2016-05-15 13:44:08 -0400

    completed add db query

--- db/db_donors.js
+++ db/db_donors.js
@@ -7,4 +7,18 @@ const pgp = require('pg-promise')({
 
 const cn = process.env.DATABASE_URL;
 const db = pgp(cn);
-const bodyParser = require('body-parser');
+
+function addDonation(req, res, next) {
+  db.one(`INSERT INTO donors (name, email, pickup_address, category, item_description)
+  VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    [req.body.name, req.body.email, req.body.pickup_address, req.body.category, req.body.item_description])
+    .then(function(data) {
+      res.rows = data;
+      next();
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+};
+
+module.exports.addDonation;
